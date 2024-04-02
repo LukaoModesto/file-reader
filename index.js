283e0f23d59c63e2c05278582ea019fc774f10eb
Lukao_Modesto <lucasoliveiractt@outlook.com>
2024-04-02 11:43:35 -0300

    file-reader

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function extrairLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map((captura) => ({
+    [captura[1]]: captura[2] }));
+    return resultados;
+}
+
 function tratarErro(erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, "Não é um arquivo."));
 }
 
-function pegarArquivo(caminhoDoArquivo){
-    const encoding = "utf-8";
-    const texto = fs.promises.readFile(caminhoDoArquivo,
-        encoding);
-        console.log(chalk.redBright(texto));
+async function pegarArquivo(caminhoDoArquivo){
+    try{
+        const encoding = "utf-8";
+        const texto = await fs.promises.readFile
+        (caminhoDoArquivo, encoding);
+
+        console.log(chalk.redBright(extrairLinks(texto)));
+        console.log(chalk.redBright(extrairLinks(texto)));
+    } catch (erro){
+        tratarErro(erro);
+    }
 }
 
 pegarArquivo("./arquivos/texto.md");
+// pegarArquivo("./arquivos/");
