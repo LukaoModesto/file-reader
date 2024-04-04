a2eced05cf8fddd8da0ccc42da3b67c868e27fa7
Lukao_Modesto <lucasoliveiractt@outlook.com>
2024-04-04 11:10:18 -0300

    rafactor: imprimir resultado

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,29 @@
-import pegarArquivo from "./index.js"; 
+import pegarArquivo from "./index.js";
 import chalk from "chalk";
-import fs from "fs"; 
+import fs from "fs";
 
 const caminho = process.argv;
 
-async function processarTexto(argumentos) {
-    
-    const caminho = argumentos[2];
-    
-    if (fs.lstatSync(caminho).isFile()) {
-        const resultado = await pegarArquivo(argumentos[2]);
-        console.log(chalk.bgWhite("lista de links"), resultado);
+function imprimirLista(resultado) {
+  console.log(chalk.bgWhite("lista de links"), resultado);
+}
 
-    }else if (fs.lstatSync(caminho).isDirectory(caminho)){
-    }
+async function processarTexto(argumentos) {
+  const caminho = argumentos[2];
+
+
+
+  if (fs.lstatSync(caminho).isFile()) {
+    const resultado = await pegarArquivo(argumentos[2]);
+    imprimirLista(resultado);
+
+  } else if (fs.lstatSync(caminho).isDirectory(caminho)) {
     const arquivos = await fs.promises.readdir(caminho);
-    console.log(arquivos);
-    
+    arquivos.forEach(async (nomeDoArquivo) => {
+      const lista = await pegarArquivo(`${caminho}/${nomeDoArquivo}`);
+      console.log(lista);
+    });
+  }
 }
 
 processarTexto(caminho);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import chalk from "chalk";
 import fs from "fs";
 
-function extrairLinks(texto){
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = [...texto.matchAll(regex)];
-    const resultados = capturas.map((captura) => ({
-    [captura[1]]: captura[2] }));
-    return resultados.length !== 0 ? resultados : "Não há links no arquivo";
+function extrairLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map((captura) => ({
+    [captura[1]]: captura[2],
+  }));
+  return resultados.length !== 0 ? resultados : "Não há links no arquivo";
 }
 
 function tratarErro(erro) {
-    console.log(erro);
-    throw new Error(chalk.red(erro.code, "Não é um arquivo."));
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, "Não é um arquivo."));
 }
 
-export default async function pegarArquivo(caminhoDoArquivo){
-    try{
-        const encoding = "utf-8";
-        const texto = await fs.promises.readFile
-        (caminhoDoArquivo, encoding);
-        return (extrairLinks(texto));
-
-    } catch (erro){
-        tratarErro(erro);
-    }
+export default async function pegarArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = "utf-8";
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    return extrairLinks(texto);
+  } catch (erro) {
+    tratarErro(erro);
+  }
 }
 
 pegarArquivo("./arquivos/texto.md");
